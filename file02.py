f06fcb7e0070249694e06c95ed2b955e2621b5dd
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-11-14 05:18:39 +0900

    File02

--- file02.py
+++ file02.py
@@ -1,3 +1,4 @@
+data=open('txt_file\data02.txt').read()
 def main(data:str):
     """
     The data is from the file. Return number of characters in the file.
@@ -6,5 +7,6 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    return len(data)
+print(main(data))
 # Read data from file
